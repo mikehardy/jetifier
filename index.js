c1fc2e6ce66c7b8b67e47bac93f583df43f3942d
Yassine Fathi <yassine.fathi@unitedremote.com>
2019-06-24 17:11:04 +0100

    changed translation engine from bash to node. https://github.com/Fluicity/jetifire/issues/1

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-/*
- 
- This is a command-line only Java program
- 
- It installs into node_modules/.bin/jetifier-standalone
- 
-*/
+const { readFileSync, writeFileSync } = require('fs');
+const { loadCSV, readDir } = require('./src/utils');
+
+const arg = process.argv.slice(2)[0];
+const mode = arg && arg === 'reverse' ? 'reverse' : 'forward';
+
+const SEARCH_DIR = 'node_modules';
+
+const csv = loadCSV();
+const files = readDir(SEARCH_DIR);
+
+for (const file of files) {
+  let data = readFileSync(file, { encoding: 'utf8' });
+  for (const c in csv) {
+    if (data.includes(mode === 'forward' ? c : csv[c])) {
+      console.log(`${mode}-jetifying: ${file}`);
+      data = mode === 'forward' ? data.replace(new RegExp(c, 'g'), csv[c]) : data.replace(new RegExp(csv[c], 'g'), c);
+      writeFileSync(file, data, { encoding: 'utf8' });
+    }
+  }
+}
---
+++ src/utils.js
@@ -0,0 +1,34 @@
+const { readFileSync, readdirSync, statSync } = require('fs');
+const { join } = require('path');
+
+var readDir = (dir, filesList = []) => {
+  const files = readdirSync(dir);
+  for (let file of files) {
+    if (statSync(dir + '/' + file).isDirectory()) {
+      filesList = readDir(dir + '/' + file, filesList);
+    }
+    else {
+      if (file.endsWith('.java') || file.endsWith('.xml') || file.endsWith('.kt')) {
+        filesList.push(dir + '/' + file);
+      }
+    }
+  }
+  return filesList;
+};
+
+const loadCSV = () => {
+  const csvFilePath = join(__dirname, 'androidx-class-mapping.csv');
+  const lines = readFileSync(csvFilePath, { encoding: 'utf8' }).split('\n');
+  const result = {};
+  for (let line of lines) {
+    const oldValue = line.split(',')[0];
+    const newValue = line.split(',')[1];
+    result[oldValue] = newValue;
+  }
+  return result;
+}
+
+module.exports = {
+  loadCSV,
+  readDir
+}
