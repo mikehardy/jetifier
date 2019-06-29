9b5d5c98004e3767c8c8c9f249db96f51c5d7c8b
Yassine Fathi <yassine.fathi@unitedremote.com>
2019-06-29 09:34:28 -0400

    feat: parallel files jetifying

---
+++ src/worker.js
@@ -0,0 +1,22 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+process.on('message', ({ filesChunk, classesMapping, mode }) => {
+
+  console.log(`${mode}-jetifying ${filesChunk.length} file(s)...`);
+
+  for (const file of filesChunk) {
+    let data = readFileSync(file, { encoding: 'utf8' });
+    for (const [oldClass, newClass] of classesMapping) {
+      if (data.includes(mode === 'forward' ? oldClass : newClass)) {
+        data = mode === 'forward' ?
+          data.replace(new RegExp(oldClass, 'g'), newClass) :
+          data.replace(new RegExp(newClass, 'g'), oldClass);
+        writeFileSync(file, data, { encoding: 'utf8' });
+      }
+    }
+  }
+
+  console.log(`${mode}-jetifyied ${filesChunk.length} file(s)`);
+
+  process.exit(0);
+});
