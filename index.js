f733fdcf44ba76eda49ed945b3f0ebd7325412c0
Julio Xavier <julioxavierr@gmail.com>
2019-07-20 02:35:49 -0300

    feat: add monorepo support

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const { fork } = require('child_process');
-const { join } = require('path');
+const { join, resolve } = require('path');
 const { getClassesMapping, readDir, chunk } = require('./src/utils');
 
 const cpus = require('os').cpus().length;
 
-const arg = process.argv.slice(2)[0];
-const mode = arg && ((arg === 'reverse') || (arg === '-r')) ? 'reverse' : 'forward';
+const args = process.argv.slice(2);
+const mode = args.find(arg => (arg === 'reverse') || (arg === '-r')) ? 'reverse' : 'forward';
+const monorepo = !!args.find(arg =>  arg === '--monorepo') || false;
 const SEARCH_DIR = 'node_modules';
 
+if (monorepo) {
+  const projectPath = resolve('.');
+  const packages = require('get-yarn-workspaces')(projectPath);
+  const packagesSearchDir = packages.map(package => join(package, SEARCH_DIR));
+  const packagesFiles = packagesSearchDir.reduce((acc, dir) => [...acc, ...readDir(dir)], []);
+}
+
 const classesMapping = getClassesMapping();
-const files = readDir(SEARCH_DIR);
+const files = [...readDir(SEARCH_DIR), ...packagesFiles];
 
 console.log(`Jetifier found ${files.length} file(s) to ${mode}-jetify. Using ${cpus} workers...`);
 
