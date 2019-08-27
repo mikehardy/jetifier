bda6de8374cafaa62737de51c58389c2660b1f80
Oleksandr Kucherenko <oleksandr.kucherenko@klarna.com>
2019-08-27 15:54:26 +0200

    cleanup

--- src/worker.js
+++ src/worker.js
@@ -2,25 +2,24 @@ const { readFileSync, writeFileSync } = require('fs');
 
 process.on('message', ({ filesChunk, classesMapping, mode, verbose }) => {
   let counter = 0;
-  let changes = 0;
   for (const file of filesChunk) {
-    if (verbose) counter++;
+    counter++;
 
+    let changes = 0;
     let data = readFileSync(file, { encoding: 'utf8' });
     for (const [oldClass, newClass] of classesMapping) {
       const left = mode === 'forward' ? oldClass : newClass;
       const right = mode === 'forward' ? newClass : oldClass;
 
       if (data.includes(left)) {
-        const newData = data.replace(new RegExp(left, 'g'), right);
-        if(verbose && newData !== data) changes++;
-        data = newData;
+        changes++;
+        data = data.replace(new RegExp(left, 'g'), right);
       }
     }
 
     writeFileSync(file, data, { encoding: 'utf8' });
 
-    if (verbose && changes > 0) process.stdout.write(`file: ${file}, changes: ${changes}, ${counter++}/${filesChunk.length}\n`);
+    if (verbose && changes > 0) process.stdout.write(`file: ${file}, changes: ${changes}, ${counter}/${filesChunk.length}\n`);
   }
 
   process.exit(0);
