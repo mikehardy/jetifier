b0ad981e956c681a96a9e05b15ae68ca0cc0cc3c
gaodeng <gaodengming@gmail.com>
2019-11-28 23:30:33 +0800

    Avoid infinite readdir loop w/symlink skip (#56)
    * skip symlink
    * nodejs 8.x

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const { existsSync, readFileSync, readdirSync, statSync } = require('fs');
+const { existsSync, readFileSync, readdirSync, statSync, lstatSync } = require('fs');
 const { join } = require('path');
 
 const chunk = (array, chunkSize) => {
@@ -14,7 +14,9 @@ const readDir = (dir, filesList = []) => {
   const files = readdirSync(dir);
   for (let file of files) {
     const filePath = join(dir, file);
-
+    if (lstatSync(filePath).isSymbolicLink()) {
+      continue;
+    }
     if (existsSync(filePath)) {
       if (statSync(filePath).isDirectory()) {
         filesList = readDir(filePath, filesList);
